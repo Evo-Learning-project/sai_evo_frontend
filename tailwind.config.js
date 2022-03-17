f573d36af3ccec4c45d409fbf8c72ce4917d94ae
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-03-17 17:32:27 +0100

    temporarily disabled tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  //purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     ripple: (theme) => ({
