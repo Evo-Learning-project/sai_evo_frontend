a5f4518a46c8aa75264ccb6bdfb1eeb7ce6a4f71
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-03-17 16:48:33 +0100

    added prod env vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,10 +19,11 @@ module.exports = {
   },
   plugins: ["vue", "@typescript-eslint"],
   rules: {
+    "no-extra-semi": "off",
     "no-unused-vars": "off",
     "vue/no-multiple-template-root": "off",
     "vue/no-v-model-argument": "off",
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
