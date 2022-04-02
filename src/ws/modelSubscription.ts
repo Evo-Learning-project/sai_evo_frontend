b3032f678628b46bc0f3fc2f07b78a405a9529bb
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-04-02 12:14:25 +0200

    applied transformation on template rules for incoming events via ws

--- src/ws/modelSubscription.ts
+++ src/ws/modelSubscription.ts
@@ -1,23 +1,38 @@
+import { convertEventTemplateRules } from "@/api/utils";
+import { Event } from "@/models";
 import store from "@/store";
 import { openAuthenticatedWsConnection } from "./utils";
 
 export const subscribeToEventChanges = (eventId: string, lock = true) => {
   const socket = openAuthenticatedWsConnection("events");
-  const message = {
+  const subscriptionMessage = {
     action: "subscribe_instance",
     pk: eventId,
-    request_id: 42,
+    request_id: 42, // TODO generate random request id
     lock,
   };
-  socket.onopen = () => socket.send(JSON.stringify(message));
+  socket.onopen = () => socket.send(JSON.stringify(subscriptionMessage));
   socket.onmessage = (m) => {
     console.log("WS MSG", JSON.parse(m.data));
     const payload = JSON.parse(m.data);
 
     if (payload.action === "update") {
+      const processedRules = convertEventTemplateRules(
+        payload.data.template?.rules
+      );
+
+      const convertedEvent = {
+        ...payload.data,
+        template: {
+          ...payload.data.template,
+          rules: processedRules,
+        },
+      } as Event;
+
+      console.log("converted event", convertedEvent);
       store.commit("teacher/setEvent", {
-        eventId: payload.data.id,
-        payload: payload.data,
+        eventId: convertedEvent.id,
+        payload: convertedEvent,
       });
     }
   };
