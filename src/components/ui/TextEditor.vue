77ff00df2e430cc28e466bf23aaf3628001d2b05
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-04-28 19:31:54 +0200

    fixed text editor ignoring tabs

--- src/components/ui/TextEditor.vue
+++ src/components/ui/TextEditor.vue
@@ -64,7 +64,7 @@ export default defineComponent({
   },
   methods: {
     onEditorChange({ quill, html, text }: unknown) {
-      //console.log('editor change!', quill, html, text)
+      //console.log("editor change!", quill, html, text);
       this.content = html;
       this.$emit("update:modelValue", html);
     },
