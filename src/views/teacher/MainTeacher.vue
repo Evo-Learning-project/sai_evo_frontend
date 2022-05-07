5071892b0a146d1d60174ee9536b05276965be93
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-05-08 00:52:18 +0200

    first implementation of new sidebar

--- src/views/teacher/MainTeacher.vue
+++ src/views/teacher/MainTeacher.vue
@@ -25,30 +25,36 @@
     <!-- FIXME review shadow here and in the other two spots in this file -->
     <div
       style="box-shadow: 2px 0 5px rgba(0, 0, 0, 0.3); z-index: 50"
-      class="flex-col justify-between hidden ease-in-out  w-96 md:block md:w-60 bg-primary"
+      class="flex-col justify-between hidden duration-200 ease-in-out  md:block bg-primary main-sidebar transition-width"
+      :class="{
+        'w-18': !hoveringSidebar,
+        'w-60 hovering-sidebar': hoveringSidebar,
+      }"
+      @mouseover="hoveringSidebar = true"
+      @mouseleave="hoveringSidebar = false"
       id="desktop-nav"
     >
-      <div
+      <!-- <div
         id="openSideBar"
-        class="absolute z-40 flex items-center justify-center hidden w-10 h-10 mt-16 -mr-10 rounded-tr rounded-br shadow cursor-pointer  left-28 top-4 text-lightText bg-primary md:hidden"
+        class="absolute z-40 flex items-center justify-center hidden w-10 h-10 mt-16 -mr-10 rounded-tr rounded-br shadow cursor-pointer left-28 top-4 text-lightText bg-primary md:hidden"
         @click="sidebarHandler(true)"
       >
         <span class="material-icons-outlined"> cancel </span>
       </div>
       <div
         id="closeSideBar"
-        class="absolute right-0 flex items-center justify-center hidden w-10 h-10 mt-16 -mr-10 rounded-tr rounded-br shadow cursor-pointer  text-lightText bg-primary"
+        class="absolute right-0 flex items-center justify-center hidden w-10 h-10 mt-16 -mr-10 rounded-tr rounded-br shadow cursor-pointer text-lightText bg-primary"
         @click="sidebarHandler(false)"
       >
         <span class="material-icons-outlined"> cancel </span>
-      </div>
+      </div>-->
       <div class="fixed h-full pr-2" style="width: inherit">
-        <div class="flex items-center w-full pl-10 pr-8 mt-4">
+        <!--<div class="flex items-center w-full pl-10 pr-8 mt-4">
           <img class="w-36" src="../../../public/unipi-logo.svg" />
         </div>
         <div
           v-if="$store.getters['shared/isAuthenticated']"
-          class="flex items-center justify-center w-full pl-2 mx-auto mt-8 mb-4 space-x-1 text-sm  text-light"
+          class="flex items-center justify-center w-full pl-2 mx-auto mt-8 mb-4 space-x-1 text-sm text-light"
         >
           <p>{{ $store.getters["shared/email"] }}</p>
           <Btn @click="logOut()" :variant="'icon'" :outline="true"
@@ -56,7 +62,7 @@
               logout
             </span></Btn
           >
-        </div>
+        </div>-->
         <ul class="flex flex-col w-full h-full mt-6">
           <router-link
             v-wave="{
@@ -74,21 +80,22 @@
             <li
               :id="'sidebar-option-' + index"
               class="
+                sidebar-link-container
                 flex
                 items-center
                 justify-between
                 w-max
-                md:w-full
+                md:w-11/12
                 px-4
-                md:pr-2 md:pl-4
                 py-2.5
-                rounded-r-full
                 cursor-pointer
                 hover:transition-colors
                 text-lightText
                 hover:bg-primary-dark hover:duration-100
               "
               :class="{
+                'rounded-r-full pl-5': hoveringSidebar,
+                'rounded-full ml-1': !hoveringSidebar,
                 'bg-primary-dark pointer-events-none': isRouteActive(option),
               }"
             >
@@ -98,37 +105,19 @@
                 >
                   {{ option.icon }}
                 </span>
-                <span class="hidden ml-4 md:inline">{{ option.label }}</span>
+                <transition name="fade-quick">
+                  <span
+                    v-show="hoveringSidebar"
+                    class="ml-4 whitespace-pre sidebar-link-label"
+                    :class="{ 'delay-0': hoveringSidebar }"
+                    >{{ option.label }}</span
+                  ></transition
+                >
               </div>
             </li>
           </router-link>
         </ul>
       </div>
-      <!-- <div class="px-8 bg-primary">
-        <div class="flex items-center px-8 mt-48 mb-4">
-          <div class="w-10 h-10 mr-3 bg-cover rounded-md">
-            <img
-              src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_5.png"
-              alt=""
-              class="w-full h-full overflow-hidden rounded-full shadow"
-            />
-          </div>
-          <div>
-            <p class="text-sm font-medium text-gray-200">Steve Doe</p>
-            <p class="text-xs text-gray-200">View Profile</p>
-          </div>
-        </div>
-        <ul class="flex items-center justify-between w-full">
-          <li class="pt-5 pb-3 cursor-pointer text-lightText">
-            
-          </li>
-          <li class="pt-5 pb-3 cursor-pointer text-lightText">
-          </li>
-          </li>
-          <li class="pt-5 pb-3 cursor-pointer text-lightText">
-          </li>
-        </ul>
-      </div>-->
     </div>
     <!--Mobile responsive sidebar-->
     <div
@@ -200,7 +189,7 @@
       </div>
     </div>
     <!-- Sidebar ends -->
-    <div class="flex flex-col w-full px-4 py-6 mx-auto md:w-4/5">
+    <div class="flex flex-col w-full px-10 py-6 mx-auto md:w-11/12">
       <h1 class="">
         {{ routeTitle }}
       </h1>
@@ -251,6 +240,7 @@ export default defineComponent({
   data() {
     return {
       showMobileSidebar: false,
+      hoveringSidebar: false,
     };
   },
   mixins: [courseIdMixin, eventIdMixin, coursePrivilegeMixin],
@@ -286,3 +276,9 @@ export default defineComponent({
   components: { ErrorView, SnackBar, Btn },
 });
 </script>
+
+<style>
+.sidebar-link-container {
+  transition: border-radius 0.2s;
+}
+</style>
