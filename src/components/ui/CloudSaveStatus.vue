12515759a623f1302f44fc30201f32bc8a4a057f
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-06-03 14:52:42 +0200

    improved autosave feedback

--- src/components/ui/CloudSaveStatus.vue
+++ src/components/ui/CloudSaveStatus.vue
@@ -1,18 +1,7 @@
 <template>
-  <div>
-    <div class="w-0 h-8 opacity-0" v-if="showOutOfViewFeedback">&nbsp;</div>
-    <div
-      class=""
-      :id="elementId"
-      :class="{
-        'fixed top-18 right-8 z-999  px-2 pt-1.5 pb-1 shadow-all-around  pr-2 rounded-full':
-          showOutOfViewFeedback,
-        'opacity-0': showOutOfViewFeedback && !fadingOutOfViewFeedback,
-        'opacity-70 transition-opacity duration-100 delay-75':
-          fadingOutOfViewFeedback,
-        'h-8': !showOutOfViewFeedback,
-      }"
-    >
+  <div :id="elementId">
+    <!-- in-view version -->
+    <div class="h-8">
       <div
         v-if="saving"
         class="flex items-center ml-auto space-x-1.5 text-muted"
@@ -42,19 +31,6 @@
           </span>
         </Tooltip>
 
-        <!--<div class="tooltip">
-        <span
-          :class="{
-            'tooltip-handle hover:bg-gray-200 hover:bg-opacity-80 rounded-md px-1.5  transition-colors duration-100': !showSaved,
-          }"
-          class="text-base cursor-default select-none opacity-80 material-icons-outlined hover:text-primary-dark"
-        >
-          cloud_done
-        </span>
-        <span class="tooltip-text tooltip-left">{{
-          $t("cloud.changes_saved_to_server")
-        }}</span>
-      </div>-->
         <p v-if="showSaved" class="mb-0.5 text-base">
           {{ $t("cloud.saved") }}
         </p>
@@ -68,6 +44,72 @@
         </div>
       </div>
     </div>
+
+    <!-- end in view version -->
+
+    <!-- out of view version -->
+    <transition name="fade-autosave">
+      <div
+        v-if="showOutOfViewFeedback"
+        class="fixed rounded-full bg-opacity-90 top-18 right-8 z-999 bg-gray-50"
+      >
+        <div
+          class="
+            opacity-70
+            shadow-all-around
+            px-3
+            pt-1.5
+            pb-1
+            pr-3
+            rounded-full
+          "
+        >
+          <div
+            v-if="saving"
+            class="flex items-center ml-auto space-x-1.5 text-muted"
+          >
+            <!-- <Spinner :size="'sm'"></Spinner> -->
+            <span
+              class="text-xl opacity-80 material-icons-outlined animate-spin"
+            >
+              sync
+            </span>
+            <p class="text-base">{{ $t("cloud.saving") }}</p>
+          </div>
+          <div
+            v-else-if="!hadError"
+            class="flex items-center ml-auto space-x-1.5 text-muted"
+          >
+            <Tooltip
+              :placement="'left'"
+              :text-value="$t('cloud.changes_saved_to_server')"
+            >
+              <span
+                :class="{
+                  'tooltip-handle hover:bg-light hover:bg-opacity-100 rounded px-1.5  transition-colors duration-100':
+                    !showSaved,
+                }"
+                class="text-xl cursor-default select-none  opacity-80 material-icons-outlined hover:text-primary"
+              >
+                cloud_done
+              </span>
+            </Tooltip>
+
+            <p v-if="showSaved" class="mb-0.5 text-base">
+              {{ $t("cloud.saved") }}
+            </p>
+          </div>
+          <div v-else>
+            <div
+              class="flex items-center space-x-1 text-sm  text-muted text-danger-dark"
+            >
+              <span class="text-sm material-icons-outlined">error_outline</span>
+              <span class="">{{ $t("cloud.error") }}</span>
+            </div>
+          </div>
+        </div>
+      </div></transition
+    >
   </div>
 </template>
 
@@ -92,38 +134,40 @@ export default defineComponent({
   },
   watch: {
     saving(newVal: boolean, oldVal: boolean) {
-      if (newVal && this.showOutOfViewFeedbackHandle !== null) {
-        clearTimeout(this.showOutOfViewFeedbackHandle);
-      }
-
       if (!newVal && oldVal) {
-        // done saving
         if (this.showSavedHandle !== null) {
-          clearTimeout(this.showSavedHandle);
+          clearInterval(this.showSavedHandle);
         }
+        if (this.showOutOfViewFeedbackHandle !== null) {
+          clearInterval(this.showOutOfViewFeedbackHandle);
+        }
+        // done saving
         this.showSaved = true;
-        this.showSavedHandle = setTimeout(() => {
-          this.showSaved = false;
-        }, 5000);
-        this.showOutOfViewFeedbackHandle = setTimeout(() => {
-          this.showOutOfViewFeedback = false;
-          this.fadingOutOfViewFeedback = false;
-        }, 1000);
+        if (!this.isElementInViewPort()) {
+          this.showOutOfViewFeedback = true;
+        }
+        this.showSavedHandle = setTimeout(() => (this.showSaved = false), 5000);
+        this.showOutOfViewFeedbackHandle = setTimeout(
+          () => (this.showOutOfViewFeedback = false),
+          1000
+        );
       }
       if (newVal && !this.isElementInViewPort()) {
-        this.showOutOfViewFeedback = true;
-        setTimeout(() => (this.fadingOutOfViewFeedback = true), 201);
+        setTimeout(() => {
+          if (this.saving) {
+            this.showOutOfViewFeedback = true;
+          }
+        }, 1000);
       }
     },
   },
   data() {
     return {
       showSaved: false,
-      elementId: uuid4(),
+      showSavedHandle: null as null | number,
       showOutOfViewFeedback: false,
-      fadingOutOfViewFeedback: false,
-      showSavedHandle: null as number | null,
-      showOutOfViewFeedbackHandle: null as number | null,
+      showOutOfViewFeedbackHandle: null as null | number,
+      elementId: uuid4(),
     };
   },
   methods: {
