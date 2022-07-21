d2353a947692341ece6b06c735de5be7a26d0ab1
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-07-21 23:42:48 +0200

    added ability to download exam instances

--- src/components/teacher/EventEditor/EventInstancesPreview.vue
+++ src/components/teacher/EventEditor/EventInstancesPreview.vue
@@ -21,11 +21,24 @@
             </SegmentedControls>
           </div>
           <div>
+            <div class="flex items-center w-full">
+              <Btn
+                :tooltip="$t('misc.download')"
+                :variant="'icon'"
+                :outline="true"
+                @click="onPrint()"
+                class="ml-auto mr-5 -mb-20"
+                :loading="saving"
+                ><span class="material-icons">save_alt</span></Btn
+              >
+            </div>
+
             <div
               class="px-8"
               v-show="instanceIndex === currentInstance"
               v-for="(_, instanceIndex) in instances"
               :key="'instance-' + instanceIndex"
+              :id="'printable-preview-' + instanceIndex"
             >
               <div
                 class="my-12"
@@ -54,6 +67,7 @@
 import { defineComponent, PropType } from "@vue/runtime-core";
 import Dialog from "@/components/ui/Dialog.vue";
 import {
+  Event,
   EventParticipationSlot,
   Exercise,
   getFakeEventParticipationSlot,
@@ -62,9 +76,21 @@ import { SelectableOption } from "@/interfaces";
 import SegmentedControls from "@/components/ui/SegmentedControls.vue";
 import AbstractEventParticipationSlot from "@/components/shared/AbstractEventParticipationSlot.vue";
 import { getTranslatedString as _ } from "@/i18n";
+import html2pdf from "html2pdf.js";
+import Btn from "@/components/ui/Btn.vue";
+import { setErrorNotification } from "@/utils";
+import { eventIdMixin } from "@/mixins";
+import { mapGetters } from "vuex";
+
 export default defineComponent({
   name: "EventInstancesPreview",
-  components: { Dialog, SegmentedControls, AbstractEventParticipationSlot },
+  mixins: [eventIdMixin],
+  components: {
+    Dialog,
+    SegmentedControls,
+    AbstractEventParticipationSlot,
+    Btn,
+  },
   props: {
     show: {
       type: Boolean,
@@ -78,10 +104,37 @@ export default defineComponent({
   data() {
     return {
       currentInstance: 0 as number,
+      saving: false,
     };
   },
-  methods: {},
+  methods: {
+    async onPrint() {
+      const element = document.getElementById(
+        "printable-preview-" + this.currentInstance
+      );
+      const worker = html2pdf();
+      this.saving = true;
+      setTimeout(async () => {
+        try {
+          await worker
+            .set({
+              filename: this.eventName + "_" + this.currentInstance + ".pdf",
+            })
+            .from(element)
+            .save();
+        } catch (e) {
+          setErrorNotification(e);
+        } finally {
+          this.saving = false;
+        }
+      }, 10);
+    },
+  },
   computed: {
+    ...mapGetters("teacher", ["event"]),
+    eventName(): string {
+      return (this.event(this.eventId) as Event).name;
+    },
     instancesAsSlotArrays(): EventParticipationSlot[][] {
       return this.instances.map((i) =>
         i.map((e) => getFakeEventParticipationSlot(e))
