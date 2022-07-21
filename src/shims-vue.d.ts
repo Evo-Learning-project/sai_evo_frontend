76acd171ee365384fcdff02efe0485cfe2256a2d
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-07-21 23:42:48 +0200

    added ability to download exam instances

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -5,6 +5,7 @@ declare module "monaco-editor-vue3";
 declare module "simple-syntax-highlighter";
 declare module "vue3-tour";
 declare module "vue-3-sanitize";
+declare module "html2pdf.js";
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
