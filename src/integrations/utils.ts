d577c74918d45a4df9f65c7b37e7deceec298df5
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-07-29 00:24:59 +0200

    fixed moodle cloze importer scores

--- src/integrations/utils.ts
+++ src/integrations/utils.ts
@@ -41,7 +41,6 @@ export const getMoodleClozeQuestionsAsExercises = (
   const answerRegex = /(=?)\s*(%[-\d]+%)?([^~]+)/;
 
   const ret = [] as Exercise[];
-  let childCount = 0;
 
   const matches = q.questiontext[0].text[0].matchAll(clozeSubQuestionRegex);
   for (const match of matches) {
@@ -52,11 +51,10 @@ export const getMoodleClozeQuestionsAsExercises = (
       text: "",
       exercise_type: ExerciseType.MULTIPLE_CHOICE_SINGLE_POSSIBLE,
       choices: [],
-      child_weight: childWeight ? parseFloat(childWeight) : undefined, // TODO fix and put 1 as default
+      child_weight: childWeight ? parseFloat(childWeight) : 1,
     };
     const answers = match[3].split("~");
     for (const answer of answers) {
-      console.log(answer);
       const answerMatch = answer.match(answerRegex) as RegExpMatchArray;
       const isCorrectAnswer = answerMatch[1] === "=";
       const answerScore = (answerMatch[2] ?? "").replace(/%/g, ""); // TODO check validity
@@ -66,26 +64,11 @@ export const getMoodleClozeQuestionsAsExercises = (
         text: answerText,
         id: "",
         // TODO fix and use 1
-        correctness: isCorrectAnswer ? 100 : parseFloat(answerScore) || 0,
+        correctness: isCorrectAnswer ? 1 : parseFloat(answerScore) / 100 || 0,
       });
     }
-    childCount++;
     ret.push(child);
   }
-  // for sub-exercises that don't have an explicitly set child_weight, set their
-  // weight so that all children are weighted evenly
-  const remainingChildrenWeight =
-    (100 -
-      ret
-        .filter((e) => typeof e.child_weight !== "undefined")
-        .map((e) => e.child_weight as number)
-        .reduce((a, b) => a + b, 0)) /
-    childCount;
-  ret.forEach((e) => {
-    if (typeof e.child_weight === "undefined") {
-      e.child_weight = remainingChildrenWeight;
-    }
-  });
   return ret;
 };
 
