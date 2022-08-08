3cd66eb78c2dce2f32fa474f0d1249edbc8571f5
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-08-08 15:42:16 +0000

    test

--- src/mixins.ts
+++ src/mixins.ts
@@ -8,156 +8,137 @@ import store from "./store/index";
 import { SharedState, StudentState } from "./store/types";
 import { setErrorNotification, setPageWideError } from "./utils";
 export const courseIdMixin = {
-  computed: {
-    courseId(): string {
-      return router.currentRoute.value.params.courseId as string;
-    },
-    currentCourse(): Course {
-      return store.getters["shared/course"](this.courseId) ?? getBlankCourse();
-    },
-  },
+	computed: {
+		courseId(): string {
+			return router.currentRoute.value.params.courseId as string;
+		},
+		currentCourse(): Course {
+			return store.getters["shared/course"](this.courseId) ?? getBlankCourse();
+		},
+	},
 };
 
 export const eventIdMixin = {
-  computed: {
-    eventId(): string {
-      return router.currentRoute.value.params.examId as string;
-    },
-    currentEvent(): string {
-      return (
-        (store.state as { student: StudentState }).student
-          .currentEventParticipation?.event?.name ?? ""
-      );
-    },
-    previewingEvent(): string {
-      return (
-        (store.state as { student: StudentState }).student.previewingEvent
-          ?.name ?? ""
-      );
-    },
-  },
+	computed: {
+		eventId(): string {
+			return router.currentRoute.value.params.examId as string;
+		},
+		currentEvent(): string {
+			return (store.state as { student: StudentState }).student.currentEventParticipation?.event?.name ?? "";
+		},
+		previewingEvent(): string {
+			return (store.state as { student: StudentState }).student.previewingEvent?.name ?? "";
+		},
+	},
 };
 
 export const texMixin = {
-  methods: {
-    triggerTexRender() {
-      const sharedState = (store.state as { shared: SharedState }).shared;
-      sharedState.dirtyTex = true;
-    },
-  },
+	methods: {
+		triggerTexRender() {
+			const sharedState = (store.state as { shared: SharedState }).shared;
+			sharedState.dirtyTex = true;
+		},
+	},
 };
 
 export const mediaQueryMixin = {
-  computed: {
-    mediaQueryMdMatches(): boolean {
-      const mq = window.matchMedia("(min-width: 768px)");
-      return mq.matches;
-    },
-  },
+	computed: {
+		mediaQueryMdMatches(): boolean {
+			const mq = window.matchMedia("(min-width: 768px)");
+			return mq.matches;
+		},
+	},
 };
 
 export const loadingMixin = {
-  methods: {
-    async withLoading(
-      callback: () => unknown,
-      onError?: (e?: unknown) => unknown,
-      onSuccess?: () => void
-    ) {
-      const sharedState = (store.state as { shared: SharedState }).shared;
+	methods: {
+		async withLoading(callback: () => unknown, onError?: (e?: unknown) => unknown, onSuccess?: () => void) {
+			const sharedState = (store.state as { shared: SharedState }).shared;
 
-      sharedState.loading = true;
-      try {
-        const ret = await callback();
-        sharedState.dirtyTex = true; // trigger tex rendering
-        onSuccess?.();
-        return ret;
-      } catch (e) {
-        onError?.(e);
-      } finally {
-        sharedState.loading = false;
-      }
-    },
-    async withFirstLoading(
-      callback: () => unknown,
-      onError = setPageWideError
-    ) {
-      const sharedState = (store.state as { shared: SharedState }).shared;
+			sharedState.loading = true;
+			try {
+				const ret = await callback();
+				sharedState.dirtyTex = true; // trigger tex rendering
+				onSuccess?.();
+				return ret;
+			} catch (e) {
+				onError?.(e);
+			} finally {
+				sharedState.loading = false;
+			}
+		},
+		async withFirstLoading(callback: () => unknown, onError = setPageWideError) {
+			const sharedState = (store.state as { shared: SharedState }).shared;
 
-      sharedState.firstLoading = true;
-      try {
-        const ret = await callback();
-        sharedState.dirtyTex = true; // trigger tex rendering
-        return ret;
-      } catch (e: any) {
-        onError?.(e);
-      } finally {
-        sharedState.firstLoading = false;
-      }
-    },
-    async withLocalLoading(callback: () => unknown) {
-      const sharedState = (store.state as { shared: SharedState }).shared;
+			sharedState.firstLoading = true;
+			try {
+				const ret = await callback();
+				sharedState.dirtyTex = true; // trigger tex rendering
+				return ret;
+			} catch (e: any) {
+				onError?.(e);
+			} finally {
+				sharedState.firstLoading = false;
+			}
+		},
+		async withLocalLoading(callback: () => unknown) {
+			const sharedState = (store.state as { shared: SharedState }).shared;
 
-      sharedState.localLoading = true;
-      try {
-        const ret = await callback();
-        sharedState.dirtyTex = true; // trigger tex rendering
-        return ret;
-      } catch (e: any) {
-        setErrorNotification(e);
-      } finally {
-        sharedState.localLoading = false;
-      }
-    },
-    setPageWideError,
-    setErrorNotification,
-  },
-  computed: {
-    ...mapState("shared", ["firstLoading", "localLoading", "loading"]),
-  },
+			sharedState.localLoading = true;
+			try {
+				const ret = await callback();
+				sharedState.dirtyTex = true; // trigger tex rendering
+				return ret;
+			} catch (e: any) {
+				setErrorNotification(e);
+			} finally {
+				sharedState.localLoading = false;
+			}
+		},
+		setPageWideError,
+		setErrorNotification,
+	},
+	computed: {
+		...mapState("shared", ["firstLoading", "localLoading", "loading"]),
+	},
 };
 
 export const savingMixin = {
-  watch: {
-    saving(newVal: boolean) {
-      (store.state as any).shared.saving = newVal;
-    },
-    savingError(newVal: boolean) {
-      (store.state as any).savingError = newVal;
-    },
-  },
+	watch: {
+		saving(newVal: boolean) {
+			(store.state as any).shared.saving = newVal;
+		},
+		savingError(newVal: boolean) {
+			(store.state as any).savingError = newVal;
+		},
+	},
 };
 
 export const coursePrivilegeMixin = {
-  methods: {
-    hasPrivileges(requiredPrivilegesList: CoursePrivilege[]) {
-      /**
-       * Given a list of required privileges, returns true iff the current
-       * user has such privileges for the current course
-       */
-      console.log(
-        "COURSE-ID hasPrivileges",
-        router.currentRoute.value.params.courseId
-      );
-      const myPrivileges: CoursePrivilege[] =
-        (store.state as any).shared.courses.find(
-          (c: Course) =>
-            c.id == (router.currentRoute.value.params.courseId as string)
-        )?.privileges ?? [];
+	methods: {
+		hasPrivileges(requiredPrivilegesList: CoursePrivilege[]) {
+			/**
+			 * Given a list of required privileges, returns true iff the current
+			 * user has such privileges for the current course
+			 */
+			const myPrivileges: CoursePrivilege[] =
+				(store.state as any).shared.courses.find(
+					(c: Course) => c.id == (router.currentRoute.value.params.courseId as string),
+				)?.privileges ?? [];
 
-      return requiredPrivilegesList.every((p) => myPrivileges.includes(p));
-    },
-    hasAnyPrivileges() {
-      console.log(
-        "COURSE-ID hasAnyPrivileges",
-        router.currentRoute.value.params.courseId
-      );
-      const myPrivileges: CoursePrivilege[] =
-        (store.state as any).shared.courses.find(
-          (c: Course) =>
-            c.id == (router.currentRoute.value.params.courseId as string)
-        )?.privileges ?? [];
+			return requiredPrivilegesList.every(p => myPrivileges.includes(p));
+		},
+		hasAnyPrivileges() {
+			// console.log(
+			//   "COURSE-ID hasAnyPrivileges",
+			//   router.currentRoute.value.params.courseId
+			// );
+			const myPrivileges: CoursePrivilege[] =
+				(store.state as any).shared.courses.find(
+					(c: Course) => c.id == (router.currentRoute.value.params.courseId as string),
+				)?.privileges ?? [];
 
-      return myPrivileges.length > 0;
-    },
-  },
+			return myPrivileges.length > 0;
+		},
+	},
 };
