7ced8caea8f1592db798b46f7f5028f7f4045f55
Andrea Simone Costa <jfet97@users.noreply.github.com>
2022-08-08 17:19:56 +0200

    eslint, prettier, husky (#4)
    * add eslint prettier v1
    
    * wip
    
    * add husky
    
    * update prettier container name

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/essential",
-    "plugin:@typescript-eslint/eslint-recommended",
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: 2022,
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
-  },
-  plugins: ["vue", "@typescript-eslint"],
-  rules: {
-    "no-extra-semi": "off",
-    "no-unused-vars": "off",
-    "vue/no-multiple-template-root": "off",
-    "vue/no-v-model-argument": "off",
-    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "plugin:vue/essential", "plugin:@typescript-eslint/eslint-recommended", "prettier"],
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly",
+	},
+	parserOptions: {
+		ecmaVersion: 2022,
+		parser: "@typescript-eslint/parser",
+		sourceType: "module",
+	},
+	plugins: ["vue", "@typescript-eslint"],
+	rules: {
+		"no-extra-semi": "off",
+		"no-unused-vars": "off",
+		"vue/no-multiple-template-root": "off",
+		"vue/no-v-model-argument": "off",
+		// "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		// "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+	},
 };
