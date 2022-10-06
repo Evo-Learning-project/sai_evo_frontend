4226e10151e9a16c017ab926070506a56c6a934f
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-10-06 20:17:51 +0000

    fixed bug with null ref

--- src/components/ui/CloudSaveStatus.vue
+++ src/components/ui/CloudSaveStatus.vue
@@ -164,11 +164,9 @@ export default defineComponent({
 			return (
 				rect.top >= 0 &&
 				rect.left >= 0 &&
-				rect.bottom <=
-					(window.innerHeight ||
-						document.documentElement.clientHeight) /* or $(window).height() */ &&
+				rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
 				rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-			); /* or $(window).width() */
+			);
 		},
 	},
 });
