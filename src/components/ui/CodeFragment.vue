b4dbb8faed47f8563edd7e8c2acb0af807c1a429
Samuele Bonini <samuelebonini@Samueles-MacBook-Pro.local>
2022-11-15 17:17:56 +0000

    implemented shared dark theme for code fragment component

--- src/components/ui/CodeFragment.vue
+++ src/components/ui/CodeFragment.vue
@@ -1,19 +1,33 @@
 <template>
 	<div class="relative">
-		<CopyToClipboard
-			v-if="!useDefault"
-			:icon-only="true"
-			:value="value"
-			:tooltip="$t('misc.copy')"
-			class="absolute top-0 right-0 z-20 semi-transparent"
-		>
-			<span
-				style="font-size: 16px !important"
-				class="material-icons-outlined text-lightText"
+		<div class="flex items-center absolute top-0 right-0 z-20 semi-transparent">
+			<Btn
+				:variant="'icon'"
+				:outline="true"
+				@click="setDark(!dark)"
+				:class="{ 'text-lightText': dark }"
 			>
-				content_copy
-			</span>
-		</CopyToClipboard>
+				<span style="font-size: 18.5px !important" class="material-icons">{{
+					dark ? "light_mode" : "dark_mode"
+				}}</span>
+			</Btn>
+			<CopyToClipboard
+				:class="{ 'text-lightText': dark }"
+				v-if="!useDefault"
+				:icon-only="true"
+				:value="value"
+				:tooltip="$t('misc.copy')"
+			>
+				<span
+					:class="{ 'text-lightText': dark }"
+					style="font-size: 16px !important"
+					class="material-icons-outlined"
+				>
+					content_copy
+				</span>
+			</CopyToClipboard>
+		</div>
+
 		<div class="absolute bottom-0 right-0 z-20 semi-transparent">
 			<Btn
 				:variant="'icon'"
@@ -46,7 +60,7 @@ import hljsVuePlugin from "@highlightjs/vue-plugin";
 // import "highlight.js/styles/github-dark.css";
 
 // TODO light theme import "highlight.js/styles/base16/material-lighter.css";
-import "highlight.js/styles/base16/material-darker.css";
+// import "highlight.js/styles/base16/material-darker.css";
 
 import hljs from "highlight.js/lib/core";
 import javascript from "highlight.js/lib/languages/javascript";
@@ -64,6 +78,8 @@ hljs.registerLanguage("cpp", cpp);
 const MAX_COLLAPSED_LENGTH = 500;
 const MAX_EXPANDED_LENGTH = 5000;
 const MAX_COLLAPSED_LINES = 20;
+
+const LOCALSTORAGE_DARK_THEME_KEY = "highlightjs-code-dark";
 
 export default defineComponent({
 	name: "CodeFragment",
@@ -95,6 +111,23 @@ export default defineComponent({
 			default: false,
 		},
 	},
+	mounted() {
+		window.addEventListener(
+			`${LOCALSTORAGE_DARK_THEME_KEY}-localstorage-changed`,
+			(event: any) => {
+				console.log("ev", event.detail);
+				this.updateDark(event.detail.storage);
+			},
+		);
+		try {
+			const isDark = JSON.parse(
+				localStorage.getItem(LOCALSTORAGE_DARK_THEME_KEY) ?? "true",
+			);
+			this.updateDark(isDark);
+		} catch {
+			this.setDark(true);
+		}
+	},
 	watch: {
 		value() {
 			// tear down and re-build component as it doesn't update on its own...
@@ -111,9 +144,38 @@ export default defineComponent({
 		return {
 			show: true,
 			expanded: false,
+			dark: false, // can't directly watch localStorage, so use a support variable
 		};
 	},
-	methods: {},
+	methods: {
+		setDark(value: boolean) {
+			localStorage.setItem(LOCALSTORAGE_DARK_THEME_KEY, JSON.stringify(value));
+			window.dispatchEvent(
+				new CustomEvent(`${LOCALSTORAGE_DARK_THEME_KEY}-localstorage-changed`, {
+					detail: {
+						storage: value,
+					},
+				}),
+			);
+			this.updateDark(value);
+		},
+		updateDark(value: boolean) {
+			const themeTitles = ["dark-highlightjs-theme", "light-highlightjs-theme"];
+			const activeTheme = value ? themeTitles[0] : themeTitles[1];
+			const removeTheme = value ? themeTitles[1] : themeTitles[0];
+			this.dark = value;
+			console.log(
+				document.querySelector(`link[title="${activeTheme}"]`),
+				document.querySelector(`link[title="${removeTheme}"]`),
+			);
+			document.querySelector(`link[title="${activeTheme}"]`)?.removeAttribute("disabled");
+			document
+				.querySelector(`link[title="${removeTheme}"]`)
+				?.setAttribute("disabled", "disabled");
+			this.show = false;
+			this.$nextTick(() => (this.show = true));
+		},
+	},
 	computed: {
 		useDefault() {
 			return this.value.trim().length === 0;
