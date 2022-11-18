928daf9939b1ddc5a9c41a3aab9497c9834b622e
Samuele <bsamuele00@gmail.com>
2022-11-18 20:22:45 +0000

    removed unused dependencies

--- src/shims-vuex.d.ts
+++ src/shims-vuex.d.ts
@@ -1,14 +1,14 @@
-// import { SharedState, TeacherState, StudentState } from "./store/types";
-// import { Store } from "vuex";
+import { SharedState, TeacherState, StudentState } from "./store/types";
+import { Store } from "vuex"; // TODO remove this
 
-// declare module "@vue/runtime-core" {
-// 	interface State {
-// 		shared: SharedState;
-// 		teacher: TeacherState;
-// 		student: StudentState;
-// 	}
-// 	interface ComponentCustomProperties {
-// 		$store: Store<State>;
-// 		$sanitize: any;
-// 	}
-// }
+declare module "@vue/runtime-core" {
+	interface State {
+		shared: SharedState;
+		teacher: TeacherState;
+		student: StudentState;
+	}
+	interface ComponentCustomProperties {
+		$store: Store<State>;
+		$sanitize: any;
+	}
+}
