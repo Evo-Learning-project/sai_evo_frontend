1cbbb92eeeac16831afb633f0af8f4aac0d2f450
Samuele Bonini <bsamuele00@gmail.com>
2023-02-12 21:53:49 +0000

    refactored route guards to factor out shared logic

--- src/router/guards.ts
+++ src/router/guards.ts
@@ -5,64 +5,56 @@ import {
 	RouteLocationRaw,
 } from "vue-router";
 
-// TODO refactor guards to factor out shared logic
-
-export const exerciseSolutionThreadBeforeGuard = async (
-	to: RouteLocationNormalized,
-	_: RouteLocationNormalized,
-	next: NavigationGuardNext,
-) => {
-	const params = to.params;
+async function hasPrivilegesOverRouteCourse(route: RouteLocationNormalized) {
+	const params = route.params;
 	const course = await getCourse(params.courseId as string);
 	const privileges = course.privileges ?? [];
-	if (privileges.length > 0) {
-		next({ name: "TeacherDetailExerciseSolutionThreads", params });
-	} else {
-		next({ name: "ExerciseSolutionThread", params });
-	}
-};
+	return privileges.length > 0;
+}
 
-export const courseTreeNodeDetailBeforeGuard = async (
-	to: RouteLocationNormalized,
-	_: RouteLocationNormalized,
-	next: NavigationGuardNext,
-) => {
-	const params = to.params;
-	const course = await getCourse(params.courseId as string);
-	const privileges = course.privileges ?? [];
-	if (privileges.length > 0) {
-		next({ name: "TeacherNodeDetail", params });
-	} else {
-		next({ name: "StudentNodeDetail", params });
-	}
-};
+/**
+ * A function that takes the name of a privileged route and the name of an unprivileged
+ * route and returns a function that can be used as a "before" route guard, which checks
+ * if the user has any permissions over the course in the route and conditionally redirects
+ * to either the privileged or the unprivileged route based on that
+ */
+const privilegedRouteWithRedirectBeforeGuard =
+	(privilegedRouteName: string, unprivilegedRouteName: string) =>
+	async (
+		to: RouteLocationNormalized,
+		_: RouteLocationNormalized,
+		next: NavigationGuardNext,
+	) => {
+		const params = to.params;
+		if (await hasPrivilegesOverRouteCourse(to)) {
+			next({ name: privilegedRouteName, params });
+		} else {
+			next({ name: unprivilegedRouteName, params });
+		}
+	};
 
-export const courseTreeBeforeGuard = async (
-	to: RouteLocationNormalized,
-	_: RouteLocationNormalized,
-	next: NavigationGuardNext,
-) => {
-	const params = to.params;
-	const course = await getCourse(params.courseId as string);
-	const privileges = course.privileges ?? [];
-	if (privileges.length > 0) {
-		next({ name: "TeacherCourseTree", params });
-	} else {
-		next({ name: "StudentCourseTree", params });
-	}
-};
+export const exerciseSolutionThreadBeforeGuard = privilegedRouteWithRedirectBeforeGuard(
+	"TeacherDetailExerciseSolutionThreads",
+	"ExerciseSolutionThread",
+);
 
-export const courseBeforeGuard = async (
-	to: RouteLocationNormalized,
-	_: RouteLocationNormalized,
-	next: NavigationGuardNext,
-) => {
-	const params = to.params;
-	const course = await getCourse(params.courseId as string);
-	const privileges = course.privileges ?? [];
-	if (privileges.length > 0) {
-		next({ name: "TeacherCourseDashboard", params });
-	} else {
-		next({ name: "StudentCourseDashboard", params });
-	}
-};
+
+export const courseTreeNodeDetailBeforeGuard = privilegedRouteWithRedirectBeforeGuard(
+	"TeacherNodeDetail",
+	"StudentNodeDetail",
+);
+
+
+
+export const courseTreeBeforeGuard = privilegedRouteWithRedirectBeforeGuard(
+	"TeacherCourseTree",
+	"StudentCourseTree",
+);
+
+
+
+export const courseBeforeGuard = privilegedRouteWithRedirectBeforeGuard(
+	"TeacherCourseDashboard",
+	"StudentCourseDashboard",
+);
+
