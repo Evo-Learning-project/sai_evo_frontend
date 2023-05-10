f667463397258595013954bf737eb6c4d2a2f58a
Samuele <bsamuele00@gmail.com>
2023-05-10 14:05:53 +0000

    created playground component

---
+++ src/views/coding_playground/CodingPlayGround.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>CodingPlayGround</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "@vue/runtime-core";
+import { courseIdMixin } from "../../mixins";
+export default defineComponent({
+	name: "CodingPlayGround",
+	mixins: [courseIdMixin],
+	props: {},
+	async created() {
+		if (!(await this.isPlaygroundEnabled())) {
+			// TODO redirect to course page
+		}
+	},
+	methods: {
+		async isPlaygroundEnabled() {
+			// TODO implement
+			return true;
+		},
+	},
+	computed: {},
+});
+</script>
+
+<style></style>
